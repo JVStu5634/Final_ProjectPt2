e380d922c2fcbe927150a6e1c42eb6df04c2703c
JVStu5634 <valdezjoseph123@gmail.com>
2026-05-01 19:35:25 -0500

    Add menu functionality for student score management
    This version introduces a menu for user interactions, including options to view saved data, add new student names, search by name, and save new data.

---
+++ main.cpp
@@ -0,0 +1,5 @@
+// Joseph Valdez
+// 4/30/2026
+//  This is an updated version of the first program that takes daily quiz scores and displays the highest and lowest scores. 
+// This program now contains a menu that allows the user to choose whether to look at the data currently saved, add new student names,
+// search for students by name and save new data added by the user.
