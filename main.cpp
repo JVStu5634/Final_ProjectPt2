18bea16e90702d0b53cc4ed853a5e6d26c66451f
JVStu5634 <valdezjoseph123@gmail.com>
2026-05-01 19:44:14 -0500

    Enhance student quiz program with menu options
    Updated program header and added new functionality for user interaction with student data.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 // Joseph Valdez
-// 4/30/2026
+// 5/1/2026
 //  This is an updated version of the first program that takes daily quiz scores and displays the highest and lowest scores. 
 // This program now contains a menu that allows the user to choose whether to look at the data currently saved, add new student names,
 // search for students by name and save new data added by the user.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct Student {
+string name;
+int score;
+};
+
+void findMinMax(Student students[], int count);
+void displayData(Student students[], int count);
+void searchEntry(Student students[], int count);
+void saveData(Student students[], int count, string filename);
+
+int main() {
+  const int MAX_SIZE = 100;
+  Student students[MAX_SIZE];
+  int count = 0;
+  string filename = "output.txt";
+
+ifstream inFile(filename);
+
+
+
+
+
+
+
